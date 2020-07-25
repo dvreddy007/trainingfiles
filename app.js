9830f4b911f3370b80a39eeea3ae7af2ae4e5091
Venkat <dvreddy007@gmail.com>
2020-07-25 23:35:48 +0530

    pushing the modules concept files

---
+++ app.js
@@ -0,0 +1,5 @@
+import { greet, PI, multiplyNumbers } from './main.js';
+
+console.log(greet); // Hello World!
+console.log(PI); // 3.14
+console.log(multiplyNumbers(6, 15)); // 90
---
+++ main.js
@@ -0,0 +1,22 @@
+let greet = "Hello World!";
+const PI = 3.14; 
+
+function multiplyNumbers(a, b) {
+    return a * b;
+}
+// The for...of Loop
+// Iterating over array
+/*let letters = ["a", "b", "c", "d", "e", "f"];
+
+for(let letter of letters) {
+    console.log(letter); // a,b,c,d,e,f
+}
+
+// Iterating over string
+let greet = "Hello World!";
+
+for(let character of greet) {
+    console.log(character); // H,e,l,l,o, ,W,o,r,l,d,!
+*/	
+// Exporting variables and functions
+export { greet, PI, multiplyNumbers };
